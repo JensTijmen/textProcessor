c98d55fc9bb07ebd1a8f0203bd367a60968bd335
Jens van Groeningen <>
2023-11-02 17:42:07 +0100

    Implementing calculateMostFrequentNWords

--- src/main/java/textprocessor/WordFrequencyAnalyzerImpl.java
+++ src/main/java/textprocessor/WordFrequencyAnalyzerImpl.java
@@ -2,9 +2,12 @@ package textprocessor;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class WordFrequencyAnalyzerImpl implements WordFrequencyAnalyzer{
     @Override
@@ -27,7 +30,25 @@ public class WordFrequencyAnalyzerImpl implements WordFrequencyAnalyzer{
 
     @Override
     public List<WordFrequency> calculateMostFrequentNWords(String text, int n) {
-        return null;
+        List<WordFrequency> wordFrequencyList = new ArrayList<>();
+        ArrayList<String> words = retrieveWordsFromText(text);
+        Set<String> wordsSet = new LinkedHashSet<>(words);
+
+        for (String word : wordsSet) {
+            WordFrequencyImpl wordFrequency = new WordFrequencyImpl(word, calculateFrequencyForWord(text, word));
+            if(!wordFrequencyList.contains(wordFrequency) ){
+                wordFrequencyList.add(wordFrequency);
+            }
+        }
+
+        wordFrequencyList.sort((o1, o2) -> {
+            Integer o1Frequency = o1.getFrequency();
+            Integer o2Frequency = o2.getFrequency();
+
+            return o2Frequency.compareTo(o1Frequency);
+        });
+
+        return wordFrequencyList.stream().limit(n).collect(Collectors.toList());
     }
 
     private ArrayList<String> retrieveWordsFromText(String text) {
